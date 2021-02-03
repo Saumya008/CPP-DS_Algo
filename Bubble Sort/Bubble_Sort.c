5baf83c93f08dedf711d4ff67b85953a6f259cec
Saumya008 <ssaumya463@gmail.com>
2021-02-03 22:16:19 +0530

    implementation in C

---
+++ Bubble Sort/Bubble_Sort.c
@@ -0,0 +1,37 @@
+#include <stdio.h>
+
+void swap(int *x, int *y)
+{
+    int temp = *x;
+    *x = *y;
+    *y = temp;
+}
+
+void BubbleSort(int arr[], int n)
+{
+    int i, j;
+    for (i = 0; i < n - 1; i++)
+
+        for (j = 0; j < n - i - 1; j++)
+            if (arr[j] > arr[j + 1])
+                swap(&arr[j], &arr[j + 1]);
+}
+
+void printArray(int arr[], int size)
+{
+    int i;
+    for (i = 0; i < size; i++)
+        printf("%d ", arr[i]);
+    printf("\n");
+}
+
+// Driver class
+int main()
+{
+    int arr[] = {98, 52, 36, 4, 82, 26, 45};
+    int n = sizeof(arr) / sizeof(arr[0]);
+    BubbleSort(arr, n);
+    printf("Sorted array: \n");
+    printArray(arr, n);
+    return 0;
+}
